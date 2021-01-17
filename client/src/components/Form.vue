4fcb987f90d666f88b0c7c94c2b58c3cab841ba1
RobinLoskli <ami0504@mail.ru>
2021-01-18 01:16:36 +1000

    form start

---
+++ client/src/components/Form.vue
@@ -0,0 +1,35 @@
+<template>
+    <form :action=action>
+        <div class="row" v-for="item in items" :key='item.name'>
+            <input :type=item.type :value=item.value :name=item.name>
+        </div>
+    </form>
+</template>
+
+<script lang="ts">
+    import {defineComponent} from "vue"
+
+    export default defineComponent({
+        data(){
+            
+            interface FormItem{
+                tagType?: string; //input or textarea
+                type: string; //check enum for this
+                value?: string | number;
+                max?: number;
+                min?: number;
+                name: string; 
+            }
+
+            const items: Array<FormItem> = [
+                {type: 'text',   name: 'login'},
+                {type: 'submit', name: 'submit', value: 'Log in'},
+            ];
+
+            return {
+                action: 'post',
+                items : items,
+            } 
+        },
+    });
+</script>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -4,6 +4,7 @@
         <div>
             <AuthNav/>
             <h1>Login</h1>
+            <Form/>
         </div>
     </div>
 </template>
@@ -11,10 +12,12 @@
 <script lang="ts">
     import {defineComponent} from 'vue';
     import AuthNav           from '../components/auth/AuthNav.vue';
+    import Form              from '../components/Form.vue';
     
     export default defineComponent({
         components: {
             AuthNav,
+            Form,
         }
     });
 
